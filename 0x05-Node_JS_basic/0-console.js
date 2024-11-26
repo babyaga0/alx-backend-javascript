2d1496567003646bf7dc37656e36c908a4fd345b
root <babyaga0>
2024-11-26 10:27:21 +0000

    r

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,9 @@
 /**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
  */
-// Arrow function
-const displayMessage = (message) => console.log(message);
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
 module.exports = displayMessage;
